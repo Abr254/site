90d089580b0a6613422e414d1db1e25085d8a110
* <kemboiabu22@gmail.com>
2025-02-02 17:23:57 +0300

    Preparing for Render deployment

--- feenax/settings.py
+++ feenax/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9we)+p_v)wyo)d+)y1pkw22vs^%$dtc%srvvy_*!p@*dr1q957'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['feenax.onrender.com']
 
 
 # Application definition
